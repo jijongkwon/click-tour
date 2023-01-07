39683197e04827738f9abbe5ad878574a2fb1eaf
jijongkwon <help3451@naver.com>
2023-01-07 20:44:58 +0900

    feat : setup click-tour

---
+++ src/main/java/com/clicktour/clicktour/ClickTourApplication.java
@@ -0,0 +1,13 @@
+package com.clicktour.clicktour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clicktour/clicktour/ClickTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clicktour.clicktour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
