03dfa642b8c0ef29752e9cbc7244d3ad6843988f
jijongkwon <help3451@naver.com>
2023-01-08 17:01:44 +0900

    feat : user colum 추가 Data : 2023 / 01 / 08 branch : main state : 미완 comment :  - id, name, nick_name, age, gender, address

--- src/main/java/com/clicktour/clicktour/domain/User.java
+++ src/main/java/com/clicktour/clicktour/domain/User.java
@@ -1,5 +1,32 @@
 package com.clicktour.clicktour.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nick_name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private String Gender;
+
+    @Column
+    private String address;
 
 }
