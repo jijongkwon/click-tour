f7bcda06c6b1e7157c775e1df93359f421711264
jijongkwon <help3451@naver.com>
2023-01-08 21:54:44 +0900

    feat : Users rep 추가 Data : 2023 / 01 / 08 branch : main state : 미완 comment :

---
+++ src/main/java/com/clicktour/clicktour/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.clicktour.clicktour.repository;
+
+import com.clicktour.clicktour.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users,Long> {
+
+}
