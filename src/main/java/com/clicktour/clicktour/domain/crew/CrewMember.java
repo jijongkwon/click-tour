e3ca89d56a92e7888503426075dfa29712a255e9
jijongkwon <help3451@naver.com>
2023-01-10 16:07:06 +0900

    feat : crewMember column 추가 Data : 2023 / 01 / 10 branch : main state : 미완 comment :  - id, state, intro, rank, crew_id, user_id

--- src/main/java/com/clicktour/clicktour/domain/crew/CrewMember.java
+++ src/main/java/com/clicktour/clicktour/domain/crew/CrewMember.java
@@ -1,4 +1,35 @@
 package com.clicktour.clicktour.domain.crew;
 
+import com.clicktour.clicktour.domain.Users;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class CrewMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Boolean state;
+
+    @Column(nullable = false)
+    private String intro;
+
+    @Column
+    private String rank;
+
+    @ManyToOne
+    @JoinColumn(name = "crew_id")
+    private Crew crew;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
+
 }
