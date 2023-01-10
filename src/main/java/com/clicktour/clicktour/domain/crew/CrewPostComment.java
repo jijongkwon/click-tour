31d639437b1634d8f9e932be210f89a0c00c0c21
jijongkwon <help3451@naver.com>
2023-01-10 16:36:54 +0900

    feat : crewPostComment column 추가 Data : 2023 / 01 / 10 branch : main state : 미완 comment :  - id, content, crew_post_id, user_id

--- src/main/java/com/clicktour/clicktour/domain/crew/CrewPostComment.java
+++ src/main/java/com/clicktour/clicktour/domain/crew/CrewPostComment.java
@@ -1,4 +1,28 @@
 package com.clicktour.clicktour.domain.crew;
 
+import com.clicktour.clicktour.domain.users.Users;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class CrewPostComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "crew_post_id")
+    private CrewPost crewPost;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
 }
