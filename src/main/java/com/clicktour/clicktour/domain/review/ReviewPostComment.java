ab1b2672062b2b2133410b34e3295a8ea8444a38
jijongkwon <help3451@naver.com>
2023-01-15 17:26:04 +0900

    feat : ReviewPostComment column 추가 Data : 2023 / 01 / 10 branch : main state : 미완 comment :  - id, user_id, review_id, comment

--- src/main/java/com/clicktour/clicktour/domain/review/ReviewPostComment.java
+++ src/main/java/com/clicktour/clicktour/domain/review/ReviewPostComment.java
@@ -1,4 +1,29 @@
 package com.clicktour.clicktour.domain.review;
 
+import com.clicktour.clicktour.domain.users.Users;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class ReviewPostComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "review_post_id")
+    private ReviewPost reviewPost;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String comment;
+
 }
