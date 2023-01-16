4f157f2df6c73df2ccb311cc351120a3e2bac2d5
jijongkwon <help3451@naver.com>
2023-01-16 21:43:38 +0900

    feat : Planner column 추가 Data : 2023 / 01 / 16 branch : main state : 미완 comment :  - id, user_id, title, start_date, end_date, intro

--- src/main/java/com/clicktour/clicktour/domain/planner/Planner.java
+++ src/main/java/com/clicktour/clicktour/domain/planner/Planner.java
@@ -1,4 +1,38 @@
 package com.clicktour.clicktour.domain.planner;
 
+import com.clicktour.clicktour.domain.users.Users;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Planner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private Date start_date;
+
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private Date end_date;
+
+    @Column
+    private String intro;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
+
 }
