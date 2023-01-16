b393c1cd20a69be6afe4b0debeebe1af13b5c7cf
jijongkwon <help3451@naver.com>
2023-01-16 21:44:22 +0900

    feat : PlannerMap column 추가 Data : 2023 / 01 / 16 branch : main state : 미완 comment :  - id, planner_id, name, memo, x, y

--- src/main/java/com/clicktour/clicktour/domain/planner/PlannerMap.java
+++ src/main/java/com/clicktour/clicktour/domain/planner/PlannerMap.java
@@ -1,4 +1,32 @@
 package com.clicktour.clicktour.domain.planner;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class PlannerMap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "planner_id")
+    private Planner planner;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String memo;
+
+    @Column(nullable = false)
+    private Float x;
+
+    @Column(nullable = false)
+    private Float y;
 }
