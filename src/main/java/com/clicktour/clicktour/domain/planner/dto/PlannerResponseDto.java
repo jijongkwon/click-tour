cebe9ef80141d3a2b989a637580a63e22f8c0b26
jijongkwon <help3451@naver.com>
2023-01-18 21:30:17 +0900

    feat : 플래너 response dto 구현 Data : 2023 / 01 / 18 branch : main state : 미완 comment :  - 응답 데이터 title, intro, start_date, end_date 추가

---
+++ src/main/java/com/clicktour/clicktour/domain/planner/dto/PlannerResponseDto.java
@@ -0,0 +1,25 @@
+package com.clicktour.clicktour.domain.planner.dto;
+
+import com.clicktour.clicktour.domain.planner.Planner;
+import com.clicktour.clicktour.domain.users.Users;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+public class PlannerResponseDto {
+
+    private String title;
+    private String intro;
+    private Date start_date;
+    private Date end_date;
+
+    public PlannerResponseDto(Planner planner){
+        this.title = planner.getTitle();
+        this.intro = planner.getIntro();
+        this.start_date = planner.getStart_date();
+        this.end_date = planner.getEnd_date();
+    }
+}
