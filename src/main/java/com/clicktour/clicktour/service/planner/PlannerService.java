578bc395f23fbdb497caeafa25555dc844234142
jijongkwon <help3451@naver.com>
2023-01-19 22:49:07 +0900

    feat : 플래너 서비스 save 기능 추가 Data : 2023 / 01 / 19 branch : main state : 미완 comment :  - 플래너 맵 db 저장 추가

--- src/main/java/com/clicktour/clicktour/service/planner/PlannerService.java
+++ src/main/java/com/clicktour/clicktour/service/planner/PlannerService.java
@@ -1,11 +1,11 @@
 package com.clicktour.clicktour.service.planner;
 
 import com.clicktour.clicktour.domain.planner.Planner;
+import com.clicktour.clicktour.domain.planner.dto.PlannerMapSaveRequestDto;
 import com.clicktour.clicktour.domain.planner.dto.PlannerResponseDto;
 import com.clicktour.clicktour.domain.planner.dto.PlannerSaveRequestDto;
-import com.clicktour.clicktour.domain.users.Users;
+import com.clicktour.clicktour.repository.PlannerMapRepository;
 import com.clicktour.clicktour.repository.PlannerRepository;
-import com.clicktour.clicktour.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,14 +14,31 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PlannerService {
     private final PlannerRepository plannerRepository;
+    private final PlannerMapRepository plannerMapRepository;
 
     @Transactional
-    public Long save(PlannerSaveRequestDto requestDto){
-        return plannerRepository.save(requestDto.toEntity()).getId();
+    public int save(PlannerSaveRequestDto plannerSaveRequestDto) {
+
+        // planner 저장
+        Planner planner = plannerRepository.save(plannerSaveRequestDto.toEntity());
+        if (planner == null) {
+            return 0;
+        }
+
+        // planner map 저장
+        PlannerMapSaveRequestDto plannerMapSaveRequestDto = new PlannerMapSaveRequestDto(
+                plannerSaveRequestDto.getPlannerMapList(), planner
+        );
+
+        if (plannerMapRepository.save(plannerMapSaveRequestDto.toEntity()).getId() == null) {
+            return 0;
+        }
+
+        return 1;
     }
 
     @Transactional
-    public PlannerResponseDto findById(Long id){
+    public PlannerResponseDto findById(Long id) {
         Planner planner = plannerRepository.findById(id).orElseThrow(() -> new
                 IllegalArgumentException("해당 플래너가 존재하지 않습니다. id :" + id));
         return new PlannerResponseDto(planner);
