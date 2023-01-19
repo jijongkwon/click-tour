b72f15137c250d5346656a55c1257659679e38a8
jijongkwon <help3451@naver.com>
2023-01-19 22:52:02 +0900

    feat : 플래너 api controller 에 responseEntity 기능 추가 Data : 2023 / 01 / 19 branch : main state : 미완 comment :  - 성공시 HttpStatus.ok  - 실패시 notFound

--- src/main/java/com/clicktour/clicktour/controller/PlannerApiController.java
+++ src/main/java/com/clicktour/clicktour/controller/PlannerApiController.java
@@ -1,9 +1,12 @@
 package com.clicktour.clicktour.controller;
 
+import com.clicktour.clicktour.domain.planner.Planner;
 import com.clicktour.clicktour.domain.planner.dto.PlannerResponseDto;
 import com.clicktour.clicktour.domain.planner.dto.PlannerSaveRequestDto;
 import com.clicktour.clicktour.service.planner.PlannerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,8 +15,11 @@ public class PlannerApiController {
     private final PlannerService plannerService;
 
     @PostMapping("/api/v1/planner")
-    public Long save(@RequestBody PlannerSaveRequestDto requestDto){
-        return plannerService.save(requestDto);
+    public ResponseEntity<Planner> save(@RequestBody PlannerSaveRequestDto requestDto){
+        if(plannerService.save(requestDto) == 0){
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/api/v1/planner/{id}")
