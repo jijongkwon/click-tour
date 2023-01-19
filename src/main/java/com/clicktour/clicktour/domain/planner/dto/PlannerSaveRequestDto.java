94028769332eaa838be60cbdac2dbee0c50b220a
jijongkwon <help3451@naver.com>
2023-01-19 22:48:18 +0900

    feat : 플래너 저장 dto 에 List 형태의 플래너 맵 필드 추가 Data : 2023 / 01 / 19 branch : main state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/domain/planner/dto/PlannerSaveRequestDto.java
+++ src/main/java/com/clicktour/clicktour/domain/planner/dto/PlannerSaveRequestDto.java
@@ -1,11 +1,13 @@
 package com.clicktour.clicktour.domain.planner.dto;
 
 import com.clicktour.clicktour.domain.planner.Planner;
+import com.clicktour.clicktour.domain.planner.PlannerMap;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -15,13 +17,16 @@ public class PlannerSaveRequestDto {
     private String intro;
     private Date start_date;
     private Date end_date;
+    private List<PlannerMap> plannerMapList;
 
     @Builder
-    public PlannerSaveRequestDto(String title, String intro, Date start_date, Date end_date) {
+    public PlannerSaveRequestDto(String title, String intro, Date start_date, Date end_date,
+                                 List<PlannerMap> plannerMapList) {
         this.title = title;
         this.intro = intro;
         this.start_date = start_date;
         this.end_date = end_date;
+        this.plannerMapList = plannerMapList;
     }
 
     public Planner toEntity() {
