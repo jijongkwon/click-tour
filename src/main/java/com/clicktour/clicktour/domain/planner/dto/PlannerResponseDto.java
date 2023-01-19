2072ea0b56953410cb0bc0bb79e1c1a0427bf612
jijongkwon <help3451@naver.com>
2023-01-19 23:06:07 +0900

    feat : 플래너 응답 dto 에 플래너 맵 리스트 추가 Data : 2023 / 01 / 19 branch : main state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/domain/planner/dto/PlannerResponseDto.java
+++ src/main/java/com/clicktour/clicktour/domain/planner/dto/PlannerResponseDto.java
@@ -1,11 +1,13 @@
 package com.clicktour.clicktour.domain.planner.dto;
 
 import com.clicktour.clicktour.domain.planner.Planner;
+import com.clicktour.clicktour.domain.planner.PlannerMap;
 import com.clicktour.clicktour.domain.users.Users;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -15,11 +17,13 @@ public class PlannerResponseDto {
     private String intro;
     private Date start_date;
     private Date end_date;
+    private List<PlannerMap> plannerMapList;
 
     public PlannerResponseDto(Planner planner){
         this.title = planner.getTitle();
         this.intro = planner.getIntro();
         this.start_date = planner.getStart_date();
         this.end_date = planner.getEnd_date();
+        this.plannerMapList = planner.getPlannerMapList();
     }
 }
