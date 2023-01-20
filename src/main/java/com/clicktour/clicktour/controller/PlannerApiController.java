086446d3dd85fed825ef95e520b26ba9bd7014b3
jijongkwon <help3451@naver.com>
2023-01-20 17:52:19 +0900

    feat : planner list 불러오기 구현 Data : 2023 / 01 / 20 branch : main state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/controller/PlannerApiController.java
+++ src/main/java/com/clicktour/clicktour/controller/PlannerApiController.java
@@ -1,5 +1,7 @@
 package com.clicktour.clicktour.controller;
 
+import com.clicktour.clicktour.domain.planner.dto.PlannerResponseDto;
+import com.clicktour.clicktour.domain.planner.dto.PlannerDetailResponseDto;
 import com.clicktour.clicktour.domain.planner.dto.PlannerSaveRequestDto;
 import com.clicktour.clicktour.service.planner.PlannerService;
 import lombok.RequiredArgsConstructor;
@@ -7,24 +9,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class PlannerApiController {
     private final PlannerService plannerService;
 
     @PostMapping("/api/v1/planner")
-    public ResponseEntity save(@RequestBody PlannerSaveRequestDto requestDto) {
-        if (plannerService.save(requestDto) == 0) {
+    public ResponseEntity<PlannerSaveRequestDto> save(@RequestBody PlannerSaveRequestDto requestDto) {
+        PlannerSaveRequestDto plannerSaveRequestDto = plannerService.save(requestDto);
+        if (plannerSaveRequestDto == null) {
             return ResponseEntity.notFound().build();
         }
-        return new ResponseEntity<>("success", HttpStatus.OK);
+        return new ResponseEntity<PlannerSaveRequestDto>(plannerSaveRequestDto, HttpStatus.OK);
     }
 
-    @GetMapping("/api/v1/planner/{id}")
-    public ResponseEntity read(@PathVariable Long id) {
-        if(plannerService.findById(id) == null){
+    @GetMapping("/planner/{id}")
+    public ResponseEntity<PlannerDetailResponseDto> readDetail(@PathVariable Long id) {
+        PlannerDetailResponseDto plannerResponseDto = plannerService.findById(id);
+        if(plannerResponseDto == null){
             return ResponseEntity.notFound().build();
         }
-        return new ResponseEntity<>("success", HttpStatus.OK);
+        return new ResponseEntity<PlannerDetailResponseDto>(plannerResponseDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/planner")
+    public ResponseEntity<List<PlannerResponseDto>> readList(){
+        List<PlannerResponseDto> plannerResponseDtoList = plannerService.findAllDesc();
+        if(plannerResponseDtoList ==  null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return new ResponseEntity<List<PlannerResponseDto>>(plannerResponseDtoList, HttpStatus.OK);
     }
 }
