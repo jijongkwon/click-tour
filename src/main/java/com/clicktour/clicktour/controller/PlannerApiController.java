c774d83e579e656e9566fe0798293b56b5228974
jijongkwon <help3451@naver.com>
2023-01-20 15:50:26 +0900

    feat : read return message 수정 Data : 2023 / 01 / 20 branch : main state : 미완 comment :  - return message : success 200  - 상태코드 : HttpStatus.OK

--- src/main/java/com/clicktour/clicktour/controller/PlannerApiController.java
+++ src/main/java/com/clicktour/clicktour/controller/PlannerApiController.java
@@ -1,6 +1,5 @@
 package com.clicktour.clicktour.controller;
 
-import com.clicktour.clicktour.domain.planner.dto.PlannerResponseDto;
 import com.clicktour.clicktour.domain.planner.dto.PlannerSaveRequestDto;
 import com.clicktour.clicktour.service.planner.PlannerService;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +21,10 @@ public class PlannerApiController {
     }
 
     @GetMapping("/api/v1/planner/{id}")
-    public PlannerResponseDto read(@PathVariable Long id) {
-        return plannerService.findById(id);
+    public ResponseEntity read(@PathVariable Long id) {
+        if(plannerService.findById(id) == null){
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity<>("Success 200", HttpStatus.OK);
     }
 }
