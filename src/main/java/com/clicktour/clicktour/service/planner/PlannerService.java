55379794b959f9516ce9b968d5df1c7b1f0a9cb2
jijongkwon <help3451@naver.com>
2023-01-20 17:52:41 +0900

    feat : planner list 불러오기 구현 Data : 2023 / 01 / 20 branch : main state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/service/planner/PlannerService.java
+++ src/main/java/com/clicktour/clicktour/service/planner/PlannerService.java
@@ -2,14 +2,18 @@ package com.clicktour.clicktour.service.planner;
 
 import com.clicktour.clicktour.domain.planner.Planner;
 import com.clicktour.clicktour.domain.planner.PlannerMap;
-import com.clicktour.clicktour.domain.planner.dto.PlannerMapSaveRequestDto;
 import com.clicktour.clicktour.domain.planner.dto.PlannerResponseDto;
+import com.clicktour.clicktour.domain.planner.dto.PlannerMapSaveRequestDto;
+import com.clicktour.clicktour.domain.planner.dto.PlannerDetailResponseDto;
 import com.clicktour.clicktour.domain.planner.dto.PlannerSaveRequestDto;
 import com.clicktour.clicktour.repository.PlannerMapRepository;
 import com.clicktour.clicktour.repository.PlannerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -18,29 +22,36 @@ public class PlannerService {
     private final PlannerMapRepository plannerMapRepository;
 
     @Transactional
-    public int save(PlannerSaveRequestDto plannerSaveRequestDto) {
+    public PlannerSaveRequestDto save(PlannerSaveRequestDto plannerSaveRequestDto) {
 
         // planner 저장
-        Planner planner =plannerRepository. save(plannerSaveRequestDto.toEntity());
+        Planner planner =plannerRepository.save(plannerSaveRequestDto.toEntity());
         if (planner.getId() == null) {
-            return 0;
+            return null;
         }
 
         // planner map 저장
         for(PlannerMap plannerMap : plannerSaveRequestDto.getPlannerMapList()){
             PlannerMapSaveRequestDto plannerMapSaveRequestDto = new PlannerMapSaveRequestDto(plannerMap, planner);
             if (plannerMapRepository.save(plannerMapSaveRequestDto.toEntity()).getId() == null) {
-                return 0;
+                return null;
             }
         }
 
-        return 1;
+        return plannerSaveRequestDto;
     }
 
     @Transactional
-    public PlannerResponseDto findById(Long id) {
+    public PlannerDetailResponseDto findById(Long id) {
         Planner planner = plannerRepository.findById(id).orElseThrow(() -> new
                 IllegalArgumentException("해당 플래너가 존재하지 않습니다. id :" + id));
-        return new PlannerResponseDto(planner);
+        return new PlannerDetailResponseDto(planner);
+    }
+
+    @Transactional
+    public List<PlannerResponseDto> findAllDesc(){
+        return plannerRepository.findAllDesc().
+                stream().map(PlannerResponseDto::new).
+                collect(Collectors.toList());
     }
 }
