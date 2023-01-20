13197f7077f761ed8b7f9ae22c5148eb3dcb9f0b
jijongkwon <help3451@naver.com>
2023-01-20 17:51:19 +0900

    feat : findAllDesc 구현 Data : 2023 / 01 / 20 branch : main state : 미완 comment :  - planner list 내림차순으로 불러오는 기능 구현

--- src/main/java/com/clicktour/clicktour/repository/PlannerRepository.java
+++ src/main/java/com/clicktour/clicktour/repository/PlannerRepository.java
@@ -2,8 +2,13 @@ package com.clicktour.clicktour.repository;
 
 import com.clicktour.clicktour.domain.planner.Planner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlannerRepository extends JpaRepository<Planner, Long> {
+    @Query("SELECT p FROM Planner p ORDER BY p.id DESC")
+    List<Planner> findAllDesc();
 }
