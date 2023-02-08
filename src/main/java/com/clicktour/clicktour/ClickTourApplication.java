3c17db34a91f181d69e4688b24b885618c2c57f2
jijongkwon <help3451@naver.com>
2023-02-08 17:22:27 +0900

    feat : enableJpaAuditing 어노테이션 추가 Data : 2023 / 02 / 08 branch : main state : 미완 comment : -

--- src/main/java/com/clicktour/clicktour/ClickTourApplication.java
+++ src/main/java/com/clicktour/clicktour/ClickTourApplication.java
@@ -2,8 +2,10 @@ package com.clicktour.clicktour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ClickTourApplication {
 
     public static void main(String[] args) {
