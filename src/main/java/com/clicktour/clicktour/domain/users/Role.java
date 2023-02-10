1ecba3631bd8dea49d622fd915c2f1f498b7219b
jijongkwon <help3451@naver.com>
2023-02-10 21:12:11 +0900

    feat : Role 클래스 추가 Data : 2023 / 02 / 10 branch : login state : 미완 comment : - 일반 사용자, 크루 리더, 손님

---
+++ src/main/java/com/clicktour/clicktour/domain/users/Role.java
@@ -0,0 +1,15 @@
+package com.clicktour.clicktour.domain.users;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    USER("ROLE_USER","일반 사용자"),
+    GUEST("ROLE_GUEST","손님"),
+    CREW_HEAD("ROLE_CREW_HEAD", "크루 리더");
+
+    private final String key;
+    private final String title;
+}
