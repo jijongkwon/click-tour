77de73edb61cd0afd4cba7c41bb33a746454c148
jijongkwon <help3451@naver.com>
2023-02-11 22:37:33 +0900

    feat : SecurityConfig 클래스 추가 Data : 2023 / 02 / 11 branch : login state : 미완 comment : -

---
+++ src/main/java/com/clicktour/clicktour/config/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.clicktour.clicktour.config.auth.security;
+
+import com.clicktour.clicktour.config.security.JwtAuthenticationFilter;
+import com.clicktour.clicktour.config.security.JwtTokenProvider;
+import com.clicktour.clicktour.domain.users.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    // 비밀번호 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /*
+       authenticate라는 인증 메소드를 제공하는 인터페이스
+       authenticationManager를 Bean 등록
+    */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .httpBasic().disable() // rest api만을 고려
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 안함
+                .and()
+                .authorizeRequests() //url 별 권환 관리 // andMatchers : 권환 관리 대상 지정
+                .antMatchers("/", "/api/v1/users/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/crew/head/**").hasRole(Role.CREW_HEAD.name())
+                .anyRequest().authenticated()
+
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣음
+
+        return http.build();
+    }
+}
+
