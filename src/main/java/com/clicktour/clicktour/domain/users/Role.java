69939d330b04c64e22d137a34918fa2f9bbaaa98
jijongkwon <help3451@naver.com>
2023-02-11 22:35:14 +0900

    refactor : 이름 변경 Data : 2023 / 02 / 11 branch : login state : 미완 comment : -

--- src/main/java/com/clicktour/clicktour/domain/users/Role.java
+++ src/main/java/com/clicktour/clicktour/domain/users/Role.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Role {
     USER("ROLE_USER","일반 사용자"),
-    GUEST("ROLE_GUEST","손님"),
+    ADMIN("ROLE_ADMIN","관리자"),
     CREW_HEAD("ROLE_CREW_HEAD", "크루 리더");
 
     private final String key;
