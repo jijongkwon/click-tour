ed067c99631d691b04c8f4132722560c77a55021
jijongkwon <help3451@naver.com>
2023-02-16 16:24:26 +0900

    feat : UserJoinRequestDto 구현 Data : 2023 / 02 / 16 branch : login state : 미완 comment :  -

---
+++ src/main/java/com/clicktour/clicktour/domain/users/dto/UserJoinRequestDto.java
@@ -0,0 +1,59 @@
+package com.clicktour.clicktour.domain.users.dto;
+
+import com.clicktour.clicktour.domain.users.Role;
+import com.clicktour.clicktour.domain.users.Users;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserJoinRequestDto {
+    private String loginId;
+    private String loginPassword;
+    private String email;
+    private String name;
+    private String nickname;
+    private int age;
+    private String gender;
+    private String address;
+    private String intro;
+    private String picture;
+    private Role role;
+
+    @Builder
+    public UserJoinRequestDto(String loginId, String loginPassword, String email, String name, String nickname,
+                              int age, String gender, String address, String intro, String picture){
+        this.loginId = loginId;
+        this.loginPassword = loginPassword;
+        this.email = email;
+        this.name = name;
+        this.nickname = nickname;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+        this.intro = intro;
+        this.picture = picture;
+    }
+
+    public Users toEntity(){
+        return Users.builder().
+                loginId(loginId).
+                loginPassword(loginPassword).
+                email(email).
+                name(name).
+                nickname(nickname).
+                age(age).
+                gender(gender).
+                address(address).
+                intro(intro).
+                picture(picture).
+                role(Role.USER).
+                build();
+    }
+
+    public void setLoginPassword(String loginPassword) {
+        this.loginPassword = loginPassword;
+    }
+}
+
