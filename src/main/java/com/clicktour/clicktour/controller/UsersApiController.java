65d87f7c7550f184921b005a3011c20ed5e7bcf3
jijongkwon <help3451@naver.com>
2023-02-16 16:25:40 +0900

    refactor : service클래스의 register함수로 구현 Data : 2023 / 02 / 16 branch : login state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/controller/UsersApiController.java
+++ src/main/java/com/clicktour/clicktour/controller/UsersApiController.java
@@ -1,9 +1,10 @@
 package com.clicktour.clicktour.controller;
 
 import com.clicktour.clicktour.config.security.JwtTokenProvider;
-import com.clicktour.clicktour.domain.users.Role;
 import com.clicktour.clicktour.domain.users.Users;
+import com.clicktour.clicktour.domain.users.dto.UserJoinRequestDto;
 import com.clicktour.clicktour.repository.UsersRepository;
+import com.clicktour.clicktour.service.users.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,38 +17,25 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/users")
-public class UsersController {
+public class UsersApiController {
 
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
     private final UsersRepository usersRepository;
+    private final UsersService usersService;
 
 
     // 회원가입
     @PostMapping("/register")
-    public Long register(@RequestBody Map<String, String> user) {
-        return usersRepository.save(Users.builder()
-                .email(user.get("email"))
-                .loginPassword(passwordEncoder.encode(user.get("loginPassword")))
-                .nickname(user.get("nickname"))
-                .intro(user.get("intro"))
-                .Gender(user.get("gender"))
-                .picture(user.get("picture"))
-                .age(Integer.parseInt(user.get("age")))
-                .loginId(user.get("loginId"))
-                .address(user.get("address"))
-                .name(user.get("name"))
-                .role(Role.USER)
-                .build()).getId();
+    public UserJoinRequestDto register(@RequestBody UserJoinRequestDto userJoinRequestDto) {
+        return usersService.register(userJoinRequestDto);
     }
 
     // 로그인
     @PostMapping("/login")
     public String login(@RequestBody Map<String, String> user) {
         Users users = usersRepository.findByLoginId(user.get("loginId"))
-                .orElseThrow(() -> new IllegalArgumentException("가입 되지 않은 이메일입니다."));
-        System.out.println(user.get("loginPassword"));
-        System.out.println(users.getLoginPassword());
+                .orElseThrow(() -> new IllegalArgumentException("가입 되지 않은 아이디입니다."));
         if (!passwordEncoder.matches(user.get("loginPassword"), users.getLoginPassword())) {
             throw new IllegalArgumentException("이메일 또는 비밀번호가 맞지 않습니다.");
         }
@@ -55,3 +43,4 @@ public class UsersController {
         return jwtTokenProvider.createToken(users.getLoginId(), users.getRole());
     }
 }
+
