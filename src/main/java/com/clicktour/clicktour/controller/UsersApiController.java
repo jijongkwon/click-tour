b617fbeba99618b22436b55aa30fbf26328a12a1
jijongkwon <help3451@naver.com>
2023-02-17 23:36:25 +0900

    feat : id, nickname 중복 찾는 기능 구현 Data : 2023 / 02 / 17 branch : login state : 미완 comment : -

--- src/main/java/com/clicktour/clicktour/controller/UsersApiController.java
+++ src/main/java/com/clicktour/clicktour/controller/UsersApiController.java
@@ -1,12 +1,13 @@
 package com.clicktour.clicktour.controller;
 
 import com.clicktour.clicktour.config.security.JwtTokenProvider;
-import com.clicktour.clicktour.domain.users.Users;
 import com.clicktour.clicktour.domain.users.dto.UserJoinRequestDto;
 import com.clicktour.clicktour.domain.users.dto.UserLoginRequestDto;
 import com.clicktour.clicktour.repository.UsersRepository;
 import com.clicktour.clicktour.service.users.UsersService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,17 +20,16 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("api/v1/users")
 public class UsersApiController {
-
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UsersRepository usersRepository;
     private final UsersService usersService;
 
 
     // 회원가입
     @PostMapping("/register")
-    public UserJoinRequestDto register(@RequestBody UserJoinRequestDto userJoinRequestDto) {
-        return usersService.register(userJoinRequestDto);
+    public ResponseEntity<?> register(@RequestBody UserJoinRequestDto userJoinRequestDto) {
+        if(usersService.register(userJoinRequestDto) == null){
+            return new ResponseEntity<>("아이디 또는 닉네임이 중복입니다.",HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(userJoinRequestDto, HttpStatus.OK);
     }
 
     // 로그인
