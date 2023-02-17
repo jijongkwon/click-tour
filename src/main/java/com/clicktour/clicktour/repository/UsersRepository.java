409ae3873ab4fe0ee481dbc468116d17d5f4cd6e
jijongkwon <help3451@naver.com>
2023-02-17 23:35:50 +0900

    feat : DB에서 nickname 찾는 기능 구현 Data : 2023 / 02 / 17 branch : login state : 미완 comment :

--- src/main/java/com/clicktour/clicktour/repository/UsersRepository.java
+++ src/main/java/com/clicktour/clicktour/repository/UsersRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByLoginId(String loginId);
+
+    Optional<Users> findByNickname(String nickname);
 }
