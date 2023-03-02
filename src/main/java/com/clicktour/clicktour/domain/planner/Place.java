dddc4d1e0187649603a875c650377ebdbc7737b2
jijongkwon <help3451@naver.com>
2023-03-02 16:15:48 +0900

    feat : place 테이블 추가 Data : 2023 / 03 / 02 branch : main state : 미완 comment : -

---
+++ src/main/java/com/clicktour/clicktour/domain/planner/Place.java
@@ -0,0 +1,36 @@
+package com.clicktour.clicktour.domain.planner;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.parameters.P;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "place")
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String place;
+
+    @ManyToOne
+    @JoinColumn(name = "planner_id")
+    private Planner planner;
+
+    @Builder
+    public Place(String place, Planner planner){
+        this.place = place;
+        this.planner = planner;
+    }
+
+    public void update(String place){
+        this.place = place;
+    }
+}
