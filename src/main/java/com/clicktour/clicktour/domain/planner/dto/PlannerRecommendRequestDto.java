cd2f895c34e8102c984f767160d1e4641f315729
jijongkwon <help3451@naver.com>
2023-03-07 13:26:47 +0900

    feat : PlannerRecommendDto 구현 Data : 2023 / 03 / 07 branch : main state : 미완 comment : -

---
+++ src/main/java/com/clicktour/clicktour/domain/planner/dto/PlannerRecommendRequestDto.java
@@ -0,0 +1,25 @@
+package com.clicktour.clicktour.domain.planner.dto;
+
+import com.clicktour.clicktour.domain.planner.Place;
+import com.clicktour.clicktour.domain.planner.Plan;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class PlannerRecommendRequestDto {
+    private Date startDate;
+    private Date endDate;
+    private String concept;
+    private List<Place> placeList;
+
+    public PlannerRecommendRequestDto(Date startDate, Date endDate, String concept, List<Place> placeList){
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.concept = concept;
+        this.placeList = placeList;
+    }
+}
