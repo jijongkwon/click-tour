4f21485ca98f7e39e779eb72c5363ae3b45ebc7e
jijongkwon <help3451@naver.com>
2023-03-07 18:10:08 +0900

    feat : BoardRepository 생성 Data : 2023 / 03 / 07 branch : board state : 미완 comment : -

---
+++ src/main/java/com/clicktour/clicktour/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.clicktour.clicktour.repository;
+
+import com.clicktour.clicktour.domain.board.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
