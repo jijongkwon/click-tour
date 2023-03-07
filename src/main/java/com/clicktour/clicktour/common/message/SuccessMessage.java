8b5326ba13131c0afc5d35fd41a60f9d6e1a41c6
jijongkwon <help3451@naver.com>
2023-03-07 18:13:27 +0900

    feat : 성공 메시지 enum 타입으로 생성 Data : 2023 / 03 / 07 branch : board state : 미완 comment : -

---
+++ src/main/java/com/clicktour/clicktour/common/message/SuccessMessage.java
@@ -0,0 +1,16 @@
+package com.clicktour.clicktour.common.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum SuccessMessage {
+
+    SUCCESS_POST(200,"success post");
+
+    private final int code;
+    private final String message;
+}
+
