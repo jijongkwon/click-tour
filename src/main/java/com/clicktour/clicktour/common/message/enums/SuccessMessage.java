97343a23f5bcab39a80547bd17aac96112bfcb80
jijongkwon <help3451@naver.com>
2023-03-08 15:27:05 +0900

    feat : 성공 메시지 추가 Data : 2023 / 03 / 08 branch : main state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/common/message/enums/SuccessMessage.java
+++ src/main/java/com/clicktour/clicktour/common/message/enums/SuccessMessage.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 public enum SuccessMessage {
 
     SUCCESS_POST_PLANNER(200,"success post planner"),
+    SUCCESS_POST_BOARD(200,"success post board"),
     SUCCESS_UPDATE_PLANNER(200, "success update planner"),
     SUCCESS_REGISTER(200, "success register"),
     SUCCESS_RECOMMEND(200,"success recommend");
