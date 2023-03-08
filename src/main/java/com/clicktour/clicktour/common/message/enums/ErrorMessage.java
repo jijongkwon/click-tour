69773b3d0594aefe87c5424b8d58f4dc8dbd05fc
jijongkwon <help3451@naver.com>
2023-03-08 16:54:43 +0900

    feat : 에러 메시지 추가 Data : 2023 / 03 / 08 branch : board state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/common/message/enums/ErrorMessage.java
+++ src/main/java/com/clicktour/clicktour/common/message/enums/ErrorMessage.java
@@ -9,7 +9,8 @@ public enum ErrorMessage {
 
     NOT_FOUND_PLANNER(403, "notFoundPlanner"),
     DUPLICATE_ID_OR_NICKNAME_OR_EMAIL(400,"duplicateIdOrNicknameOrEmail"),
-    NOT_FOUND_ID(403, "notFoundID"),
+    NOT_FOUND_ID(403, "not found id"),
+    NOT_FOUND_USER(403, "not found user"),
     MISMATCH_PASSWORD(400, "mismatchPassword");
 
     private final int code;
