ed169a32c461a62bdf4480a1e57a9c2adcb63646
jijongkwon <help3451@naver.com>
2023-03-09 17:28:26 +0900

    feat : 에러메시지 추가 Data : 2023 / 03 / 09 branch : board state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/common/message/enums/ErrorMessage.java
+++ src/main/java/com/clicktour/clicktour/common/message/enums/ErrorMessage.java
@@ -7,10 +7,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorMessage {
 
-    NOT_FOUND_PLANNER(403, "notFoundPlanner"),
+    NOT_FOUND_PLANNER(404, "notFoundPlanner"),
     DUPLICATE_ID_OR_NICKNAME_OR_EMAIL(400,"duplicateIdOrNicknameOrEmail"),
-    NOT_FOUND_ID(403, "not found id"),
-    NOT_FOUND_USER(403, "not found user"),
+    NOT_FOUND_ID(404, "not found id"),
+    NOT_FOUND_USER(404, "not found user"),
+    NOT_FOUND_BOARD(404, "not found board"),
     MISMATCH_PASSWORD(400, "mismatchPassword"),
     FORBIDDEN(403, "Forbidden");
 
