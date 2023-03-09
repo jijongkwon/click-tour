c4b7725f997630e06b91b548e901de5ace3d536d
jijongkwon <help3451@naver.com>
2023-03-09 17:28:06 +0900

    feat : 게시판 목록 조회 구현 및 save 함수 리펙토링 Data : 2023 / 03 / 09 branch : board state : 미완 comment :  - CheckService를 이용해 가독성을 높힘

--- src/main/java/com/clicktour/clicktour/controller/BoardController.java
+++ src/main/java/com/clicktour/clicktour/controller/BoardController.java
@@ -1,17 +1,21 @@
 package com.clicktour.clicktour.controller;
 
+import com.clicktour.clicktour.common.message.dto.ExceptionDto;
+import com.clicktour.clicktour.common.message.enums.ErrorMessage;
 import com.clicktour.clicktour.common.message.enums.SuccessMessage;
 import com.clicktour.clicktour.common.message.dto.ResponseDto;
+import com.clicktour.clicktour.common.service.CheckService;
 import com.clicktour.clicktour.domain.board.Board;
+import com.clicktour.clicktour.domain.board.dto.BoardResponseDto;
 import com.clicktour.clicktour.domain.board.dto.BoardSaveRequestDto;
 import com.clicktour.clicktour.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,9 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 
     private final BoardService boardService;
+    private final CheckService checkService;
 
     @PostMapping("/post")
-    public ResponseEntity<?> save(@RequestBody BoardSaveRequestDto requestDto){
+    public ResponseEntity<?> save(@RequestBody BoardSaveRequestDto requestDto, HttpServletRequest httpServletRequest){
+
+        if(checkService.checkJwtToken(httpServletRequest.getHeader("X-AUTH-TOKEN"))) {
+            return new ResponseEntity<>(new ExceptionDto(ErrorMessage.FORBIDDEN), HttpStatus.FORBIDDEN);
+        }
+
         Board board = boardService.save(requestDto);
 
         if (board == null) {
@@ -29,5 +39,14 @@ public class BoardController {
         }
 
         return new ResponseEntity<>(new ResponseDto(SuccessMessage.SUCCESS_POST_BOARD), HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    private ResponseEntity<?> readBoardList(){
+        List<BoardResponseDto> boardResponseDtoList = boardService.readBoardList();
+        if(boardResponseDtoList.isEmpty()){
+            return new ResponseEntity<>(new ExceptionDto(ErrorMessage.NOT_FOUND_BOARD), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(boardService.readBoardList(), HttpStatus.OK);
     }
 }
