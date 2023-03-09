185d053cd6e3955699ef9a7f047eedf24aaf4ec1
jijongkwon <help3451@naver.com>
2023-03-09 17:25:17 +0900

    feat : 모든 게시판 찾는 함수 구현 Data : 2023 / 03 / 09 branch : board state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/repository/BoardRepository.java
+++ src/main/java/com/clicktour/clicktour/repository/BoardRepository.java
@@ -1,9 +1,16 @@
 package com.clicktour.clicktour.repository;
 
 import com.clicktour.clicktour.domain.board.Board;
+import com.clicktour.clicktour.domain.board.dto.BoardResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Query("SELECT b FROM Board b ORDER BY b.id DESC")
+    List<Board> findAllDesc();
 }
