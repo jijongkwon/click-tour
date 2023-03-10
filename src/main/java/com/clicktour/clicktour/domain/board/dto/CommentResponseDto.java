f453dcfea03e737ff685281a7e31ce93660c6153
jijongkwon <help3451@naver.com>
2023-03-10 23:17:16 +0900

    refactor : 댓글 응답 데이터 삭제 Data : 2023 / 03 / 10 branch : board state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/domain/board/dto/CommentResponseDto.java
+++ src/main/java/com/clicktour/clicktour/domain/board/dto/CommentResponseDto.java
@@ -9,21 +9,15 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 public class CommentResponseDto {
-    private Long id;
     private String comment;
     private LocalDate createdDate;
     private LocalDate modifiedDate;
     private String nickname;
-    private Long board_id;
-    private Long user_id;
 
     public CommentResponseDto(Comments comments){
-        this.id = comments.getId();
         this.comment = comments.getComment();
         this.createdDate = comments.getCreatedDate();
         this.modifiedDate = comments.getModifiedDate();
         this.nickname = comments.getUsers().getNickname();
-        this.board_id = comments.getBoard().getId();
-        this.user_id = comments.getUsers().getId();
     }
 }
