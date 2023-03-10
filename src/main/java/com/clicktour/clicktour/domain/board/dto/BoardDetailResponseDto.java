ff71fc15dd7afc78ab084f49fc3dfdbffa6c4f93
jijongkwon <help3451@naver.com>
2023-03-10 23:14:45 +0900

    feat : 게시물 세부 정보 보기에 댓글 추가 Data : 2023 / 03 / 10 branch : board state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/domain/board/dto/BoardDetailResponseDto.java
+++ src/main/java/com/clicktour/clicktour/domain/board/dto/BoardDetailResponseDto.java
@@ -4,6 +4,9 @@ import com.clicktour.clicktour.domain.board.Board;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 @NoArgsConstructor
 public class BoardDetailResponseDto {
@@ -11,10 +14,13 @@ public class BoardDetailResponseDto {
     private String title;
     private String nickname;
     private String content;
+    private List<CommentResponseDto> commentResponseDtoList;
 
     public BoardDetailResponseDto(Board board){
         this.title = board.getTitle();
         this.nickname = board.getUsers().getNickname();
         this.content = board.getContent();
+        this.commentResponseDtoList = board.getComments().
+                stream().map(CommentResponseDto::new).collect(Collectors.toList());
     }
 }
