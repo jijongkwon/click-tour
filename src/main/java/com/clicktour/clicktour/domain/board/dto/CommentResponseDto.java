315ba253c17d76d3ba70228c03d515d4ec0cd215
jijongkwon <help3451@naver.com>
2023-03-16 17:16:51 +0900

    feat : 댓글 응답 dto에 id 변수 추가 Data : 2023 / 03 / 16 branch : board state : 미완 comment :  - 댓글 업데이트를 위한 id 추가

--- src/main/java/com/clicktour/clicktour/domain/board/dto/CommentResponseDto.java
+++ src/main/java/com/clicktour/clicktour/domain/board/dto/CommentResponseDto.java
@@ -9,12 +9,14 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 public class CommentResponseDto {
+    private Long id;
     private String comment;
     private LocalDate createdDate;
     private LocalDate modifiedDate;
     private String nickname;
 
     public CommentResponseDto(Comments comments){
+        this.id = comments.getId();
         this.comment = comments.getComment();
         this.createdDate = comments.getCreatedDate();
         this.modifiedDate = comments.getModifiedDate();
