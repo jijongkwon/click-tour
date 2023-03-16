3a8a49922c8473fc2211e84946e511f7f2d96572
jijongkwon <help3451@naver.com>
2023-03-16 17:16:05 +0900

    feat : 에러 메시지 추가 Data : 2023 / 03 / 16 branch : board state : 미완 comment :  - 댓글을 찾을 수 없다는 메시지 추가

--- src/main/java/com/clicktour/clicktour/common/message/enums/ErrorMessage.java
+++ src/main/java/com/clicktour/clicktour/common/message/enums/ErrorMessage.java
@@ -7,11 +7,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorMessage {
 
-    NOT_FOUND_PLANNER(404, "notFoundPlanner"),
+    NOT_FOUND_PLANNER(404, "not found planner"),
     DUPLICATE_ID_OR_NICKNAME_OR_EMAIL(400,"duplicateIdOrNicknameOrEmail"),
     NOT_FOUND_ID(404, "not found id"),
     NOT_FOUND_USER(404, "not found user"),
     NOT_FOUND_BOARD(404, "not found board"),
+    NOT_FOUND_COMMENT(404, "not found comment"),
     MISMATCH_PASSWORD(400, "mismatchPassword"),
     FORBIDDEN(403, "Forbidden");
 
