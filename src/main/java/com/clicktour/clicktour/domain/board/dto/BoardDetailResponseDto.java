b6f6bbb3499890a2b209e6b16d91c820d8f3f5bd
jijongkwon <help3451@naver.com>
2023-03-20 14:39:18 +0900

    feat : 게시글 생성시간, 수정시간 응답 추가 Data : 2023 / 03 / 20 branch : main state : 미완 comment :  -

--- src/main/java/com/clicktour/clicktour/domain/board/dto/BoardDetailResponseDto.java
+++ src/main/java/com/clicktour/clicktour/domain/board/dto/BoardDetailResponseDto.java
@@ -4,6 +4,7 @@ import com.clicktour.clicktour.domain.board.Board;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,12 +15,16 @@ public class BoardDetailResponseDto {
     private String title;
     private String nickname;
     private String content;
+    private LocalDate createdDate;
+    private LocalDate modifiedDate;
     private List<CommentResponseDto> commentResponseDtoList;
 
     public BoardDetailResponseDto(Board board){
         this.title = board.getTitle();
         this.nickname = board.getUsers().getNickname();
         this.content = board.getContent();
+        this.createdDate = board.getCreatedDate();
+        this.modifiedDate = board.getModifiedDate();
         this.commentResponseDtoList = board.getComments().
                 stream().map(CommentResponseDto::new).collect(Collectors.toList());
     }
--- src/main/java/com/clicktour/clicktour/domain/board/dto/BoardResponseDto.java
+++ src/main/java/com/clicktour/clicktour/domain/board/dto/BoardResponseDto.java
@@ -4,16 +4,24 @@ import com.clicktour.clicktour.domain.board.Board;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 public class BoardResponseDto {
     private String title;
     private String nickname;
     private int view;
+    private LocalDate createdDate;
+    private LocalDate modifiedDate;
+
 
     public BoardResponseDto(Board board){
         this.title = board.getTitle();
         this.nickname = board.getUsers().getNickname();
         this.view = board.getView();
+        this.createdDate = board.getCreatedDate();
+        this.modifiedDate = board.getModifiedDate();
     }
 }
