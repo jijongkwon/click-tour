d6878616d848b7c04f9f81b7b9e504f9cd5577d3
jijongkwon <help3451@naver.com>
2023-05-24 16:52:01 +0900

    test : 회원가입, 로그인, 유저정보 불러오는 테스트를 위한 메서드 생성

---
+++ src/test/java/com/clicktour/clicktour/service/users/UsersServiceTest.java
@@ -0,0 +1,20 @@
+package com.clicktour.clicktour.service.users;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersServiceTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void getUserInfo() {
+    }
+}
