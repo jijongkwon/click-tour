892be352d6ec111d6e07480486ccbaa86154fbd9
jijongkwon <help3451@naver.com>
2023-05-24 17:03:08 +0900

    test : 게시판 기능 테스트 초기화

---
+++ src/test/java/com/clicktour/clicktour/controller/BoardApiControllerTest.java
@@ -0,0 +1,36 @@
+package com.clicktour.clicktour.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardApiControllerTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void saveComments() {
+    }
+
+    @Test
+    void readBoardList() {
+    }
+
+    @Test
+    void readBoardDetail() {
+    }
+
+    @Test
+    void updateBoard() {
+    }
+
+    @Test
+    void updateComments() {
+    }
+
+    @Test
+    void deleteBoard() {
+    }
+}
---
+++ src/test/java/com/clicktour/clicktour/service/board/BoardServiceTest.java
@@ -0,0 +1,40 @@
+package com.clicktour.clicktour.service.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardServiceTest {
+
+    @Test
+    void saveBoard() {
+    }
+
+    @Test
+    void readBoardList() {
+    }
+
+    @Test
+    void readDetailBoard() {
+    }
+
+    @Test
+    void saveComments() {
+    }
+
+    @Test
+    void updateBoard() {
+    }
+
+    @Test
+    void updateComment() {
+    }
+
+    @Test
+    void deleteBoard() {
+    }
+
+    @Test
+    void deleteComment() {
+    }
+}
