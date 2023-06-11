9f303a5ed2c7227443a19f8b8612fb806152af51
jijongkwon <help3451@naver.com>
2023-06-11 22:32:58 +0900

    feat : 아이디, 이메일, 닉네임 유효성 체크  validator 생성

---
+++ src/main/java/com/clicktour/clicktour/common/validators/RegisterValidator.java
@@ -0,0 +1,52 @@
+package com.clicktour.clicktour.common.validators;
+
+import com.clicktour.clicktour.domain.users.dto.UserJoinRequestDto;
+import com.clicktour.clicktour.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component // 빈 등록
+@RequiredArgsConstructor
+public class RegisterValidator implements Validator {
+
+    private final UsersRepository usersRepository;
+
+    /**
+     * supports() 메서드는 Validator 인터페이스의 메서드로서, 주어진 클래스가 Validator가 유효성 검사를 수행할 수 있는 대상인지 여부를 결정
+     * @param clazz the {@link Class} that this {@link Validator} is
+     * being asked if it can {@link #validate(Object, Errors) validate}
+     * @return boolean
+     */
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return UserJoinRequestDto.class.equals(clazz);
+    }
+
+    /**
+     * 중복 체크
+     * @param target the object that is to be validated
+     * @param errors contextual state about the validation process
+     */
+    @Override
+    public void validate(Object target, Errors errors) {
+        UserJoinRequestDto userJoinRequestDto = (UserJoinRequestDto) target;
+
+        // 이메일 중복 체크
+        if (usersRepository.existsByEmail(userJoinRequestDto.getEmail())) {
+            errors.reject(userJoinRequestDto.getEmail(), "이미 사용중인 이메일입니다.");
+        }
+
+        // 아이디 중복 체크
+        if (usersRepository.existsByLoginId(userJoinRequestDto.getLoginId())) {
+            errors.reject(userJoinRequestDto.getLoginId(), "이미 사용중인 아이디입니다.");
+        }
+
+        // 닉네임 중복 체크
+        if (usersRepository.existsByNickname(userJoinRequestDto.getNickname())) {
+            errors.reject(userJoinRequestDto.getNickname(), "이미 사용중인 닉네임입니다.");
+        }
+    }
+}
