da89bbc52994cfc3f61362b3038b2208b2a345b6
jijongkwon <help3451@naver.com>
2023-06-13 17:39:17 +0900

    fix: 단위 테스트에서 통합테스트로 변경 - 데이터베이스와 상호작용이 있으므로 통합테스트로 진행

--- src/test/java/com/clicktour/clicktour/service/users/UsersServiceTest.java
+++ src/test/java/com/clicktour/clicktour/service/users/UsersServiceTest.java
@@ -1,61 +1,58 @@
 package com.clicktour.clicktour.service.users;
 
+import com.clicktour.clicktour.common.validators.RegisterValidator;
 import com.clicktour.clicktour.domain.users.Users;
 import com.clicktour.clicktour.domain.users.dto.UserJoinRequestDto;
 import com.clicktour.clicktour.repository.UsersRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class UsersServiceTest {
-    String loginId = "id";
-    String name = "name";
-    String nickname = "test";
-    String email = "test@test.com";
-    String loginPassword = "pw";
-    int age = 1;
-    String gender = "man";
-    @InjectMocks
+    @Autowired
     private UsersService usersService;
-    @Mock
+    @Autowired
     private UsersRepository usersRepository;
-    @Mock
+    @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private RegisterValidator registerValidator;
 
     @Test
     void register() {
         // given
         UserJoinRequestDto userJoinRequestDto = UserJoinRequestDto.builder()
-                .loginId(loginId)
-                .name(name)
-                .nickname(nickname)
-                .email(email)
-                .loginPassword(loginPassword)
-                .age(age)
-                .gender(gender)
+                .loginId("id")
+                .name("name")
+                .nickname("test")
+                .email("test@email.com")
+                .loginPassword("pw")
+                .age(3)
+                .gender("man")
                 .build();
 
         // when
-        usersService.register(userJoinRequestDto);
+        BindingResult bindingResult = new BeanPropertyBindingResult(userJoinRequestDto, "userJoinRequestDto");
+        usersService.register(userJoinRequestDto, bindingResult);
 
-        Users users = usersRepository.findByNickname("test").orElseThrow(() ->
+        Users users = usersRepository.findByNickname(userJoinRequestDto.getNickname()).orElseThrow(() ->
                 new IllegalArgumentException("닉네임이 일치하지 않음"));
 
         // then
-        assertEquals(loginId, users.getLoginId());
-        assertEquals(name, users.getName());
-        assertEquals(nickname, users.getNickname());
-        assertEquals(email, users.getEmail());
-        assertEquals(age, users.getAge());
-        assertEquals(gender, users.getGender());
-        boolean passwordMatches = passwordEncoder.matches(loginPassword, users.getLoginPassword());
+        assertEquals("id", users.getLoginId());
+        assertEquals("name", users.getName());
+        assertEquals("test", users.getNickname());
+        assertEquals("test@email.com", users.getEmail());
+        assertEquals(3, users.getAge());
+        assertEquals("man", users.getGender());
+        boolean passwordMatches = passwordEncoder.matches("pw", users.getLoginPassword());
         assertTrue(passwordMatches);
     }
 
