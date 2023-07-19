e4f8bd8da588710b189f3276efa4c58265e97191
jijongkwon <help3451@naver.com>
2023-07-19 19:02:07 +0900

    refactor : 사용하지 않는 역할 제거

--- src/main/java/com/clicktour/clicktour/domain/users/Role.java
+++ src/main/java/com/clicktour/clicktour/domain/users/Role.java
@@ -7,8 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Role {
     USER("ROLE_USER", "일반 사용자"),
-    ADMIN("ROLE_ADMIN", "관리자"),
-    CREW_HEAD("ROLE_CREW_HEAD", "크루 리더");
+    ADMIN("ROLE_ADMIN", "관리자");
 
     private final String key;
     private final String title;
