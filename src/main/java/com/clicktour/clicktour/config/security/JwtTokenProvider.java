d484ab79084f6fe52aa72cbd178f34047fee4191
jijongkwon <help3451@naver.com>
2023-07-24 16:57:37 +0900

    refactor : @Value 사용으로 보안성 높임

--- src/main/java/com/clicktour/clicktour/config/security/JwtTokenProvider.java
+++ src/main/java/com/clicktour/clicktour/config/security/JwtTokenProvider.java
@@ -6,12 +6,14 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
 import java.util.Date;
@@ -21,11 +23,16 @@ import java.util.Date;
 public class JwtTokenProvider {
 
     private final UserDetailsService userDetailsService;
-    // 토큰 유효시간 12시간
-    private final long tokenValidTime = 30 * 60 * 1000L;
-    private String secretKey = "test";
+
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
+    @Value("${spring.jwt.accessValidTime}")
+    private long accessTokenValidTime;
+    @Value("${spring.jwt.refreshValidTime}")
+    private long refreshTokenValidTime;
 
     // 객체 초기화, secretKey를 Base64로 인코딩
+    @PostConstruct
     protected void init() {
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
     }
@@ -38,7 +45,7 @@ public class JwtTokenProvider {
         return Jwts.builder()
                 .setClaims(claims) // 정보 저장
                 .setIssuedAt(now) // 토큰 발행 시간 정보
-                .setExpiration(new Date(now.getTime() + tokenValidTime)) // set Expire Time
+                .setExpiration(new Date(now.getTime() + accessTokenValidTime)) // set Expire Time
                 .signWith(SignatureAlgorithm.HS256, secretKey) // 사용할 암호화 알고리즘과 signature에 들어갈 secret 값 세팅
                 .compact();
     }
