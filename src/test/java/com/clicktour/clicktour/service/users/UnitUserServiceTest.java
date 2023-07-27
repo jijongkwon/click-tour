8ee4d82ac0a9edea2b5a781b042a8eb1c10020d6
jijongkwon <help3451@naver.com>
2023-07-27 20:20:37 +0900

    test : userService 단위테스트 클래스 생성

---
+++ src/test/java/com/clicktour/clicktour/service/users/UnitUserServiceTest.java
@@ -0,0 +1,10 @@
+package com.clicktour.clicktour.service.users;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UnitUserServiceTest {
+
+}
