c6a9ee7810f7badb3030317389be5f4eb7f76855
jijongkwon <help3451@naver.com>
2023-07-27 20:21:17 +0900

    test : 아이디 확인 테스트 구현

--- src/test/java/com/clicktour/clicktour/service/users/UnitUserServiceTest.java
+++ src/test/java/com/clicktour/clicktour/service/users/UnitUserServiceTest.java
@@ -1,10 +1,42 @@
 package com.clicktour.clicktour.service.users;
 
 
+import com.clicktour.clicktour.domain.users.Users;
+import com.clicktour.clicktour.repository.UsersRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UnitUserServiceTest {
 
+    @InjectMocks
+    private UsersService usersService;
+
+    @Mock
+    private UsersRepository usersRepository;
+
+    @Test
+    void 아이디_화인(){
+        // given
+        String loginId = "test";
+        Users mockUsers = new Users();
+
+        when(usersRepository.findByLoginId(loginId)).thenReturn(Optional.of(mockUsers));
+
+        // when
+        Users result = usersService.checkId(loginId);
+
+        // then
+        assertNotNull(result);
+        assertEquals(mockUsers,result);
+    }
 }
