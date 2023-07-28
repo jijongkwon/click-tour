1ae74c0bc5fa44dcead8ac5bad14c676899142ca
jijongkwon <help3451@naver.com>
2023-07-28 15:48:50 +0900

    refactor : 생성자 직접생성 대신 어노테이션 사용

--- src/main/java/com/clicktour/clicktour/domain/users/dto/UserLoginRequestDto.java
+++ src/main/java/com/clicktour/clicktour/domain/users/dto/UserLoginRequestDto.java
@@ -1,18 +1,16 @@
 package com.clicktour.clicktour.domain.users.dto;
 
-import com.clicktour.clicktour.domain.users.Users;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserLoginRequestDto {
 
     private String loginId;
     private String loginPassword;
-
-    public UserLoginRequestDto(Users users) {
-        this.loginId = users.getLoginId();
-        this.loginPassword = users.getLoginPassword();
-    }
 }
